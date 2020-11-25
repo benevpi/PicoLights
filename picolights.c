8fe532d37b46968469d8648c90f4e1c79bc29cd4
benevpi <ben.everard@raspberrypi.org>
2020-11-25 16:08:25 +0000

    updated and now working
    arrgh, 310 should have been 400.

--- picolights.c
+++ picolights.c
@@ -14,14 +14,15 @@
 #include "pico/multicore.h"
 
 #define STRING_LEN 32
-int bit_depth=12;
-const int PIN_TX = 0;
 
-uint pixels[STRING_LEN];
-uint errors[STRING_LEN];
+int bit_depth=12;
+const int PIN_TX = 1;
+
+uint32_t pixels[STRING_LEN];
+uint32_t errors[STRING_LEN];
 
 static inline void put_pixel(uint32_t pixel_grb) {
-    pio_put_blocking(pio0, 0, pixel_grb << 8u);
+    pio_sm_put_blocking(pio0, 0, pixel_grb << 8u);
 }
 
 static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b) {
@@ -31,45 +32,57 @@ static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b) {
             (uint32_t) (b);
 }
 
-int ws2812b_core() {
+void ws2812b_core() {
 	int value;
 	int shift = bit_depth-8;
 	
     while (1){
 		//let's keep this simple and just do it all white will have to be a bit more careful about the timings when doing RGB
+		/**
 		for(int i=0; i<STRING_LEN; i++) {
 			value=(pixels[i] + errors[i]) >> shift;
 			put_pixel(urgb_u32(0, 0, value));
 			errors[i] = (pixels[i] + errors[i]) - (value << shift); // is this correct?
 		}
-		sleep_us(310);
-	}		
+		**/
+		for(int i=0; i<STRING_LEN; i++) {
+			value=(pixels[i] + errors[i]) >> shift;
+			//printf("pixels[i] = %d", pixels[i]);
+			put_pixel(urgb_u32(0, 0, value));
+			errors[i] = (pixels[i] + errors[i]) - (value << shift); // is this correct?
+		}
+		sleep_us(400);
+	}
+}	
 		
-}
-
 
 
 int main() {
 	
+	setup_default_uart();
+	
 	PIO pio = pio0;
     int sm = 0;
     uint offset = pio_add_program(pio, &ws2812_program);
-    ws2812_program_init(pio, sm, offset, PIN_TX, 1000000, false);
+    ws2812_program_init(pio, sm, offset, PIN_TX, 800000, false);
+	
+	for(int i=0; i< STRING_LEN; ++i) {
+		pixels[i]=0;
+	}
  
 	multicore_launch_core1(ws2812b_core);
    
     while (1) {
         for (int i = 0; i < 30; ++i) {
-
 			
 			pixels[i] = i;
 			
-			for (int j=0;j<30;++j){
+			for (int j=0;j<100;++j){
 				pixels[0] = j;
 				if(j%8 == 0) { pixels[1] = j; }
 				sleep_ms(50);
 			}
-			for (int j=30;j>0;--j){
+			for (int j=100;j>0;--j){
 				pixels[0] = j;
 				if(j%8 == 0) { pixels[1] = j; }
 				sleep_ms(50);
