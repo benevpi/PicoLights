9f5157dfbd9ad6d92974a2012ad62ff831184a0c
benevpi <ben.everard@raspberrypi.org>
2020-11-25 16:20:48 +0000

    now working in full RGB

--- picolights.c
+++ picolights.c
@@ -18,8 +18,14 @@
 int bit_depth=12;
 const int PIN_TX = 1;
 
-uint32_t pixels[STRING_LEN];
-uint32_t errors[STRING_LEN];
+uint32_t pixelsb[STRING_LEN];
+uint32_t errorsb[STRING_LEN];
+
+uint32_t pixelsr[STRING_LEN];
+uint32_t errorsr[STRING_LEN];
+
+uint32_t pixelsg[STRING_LEN];
+uint32_t errorsg[STRING_LEN];
 
 static inline void put_pixel(uint32_t pixel_grb) {
     pio_sm_put_blocking(pio0, 0, pixel_grb << 8u);
@@ -33,23 +39,20 @@ static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b) {
 }
 
 void ws2812b_core() {
-	int value;
+	int valuer, valueg, valueb;
 	int shift = bit_depth-8;
 	
     while (1){
-		//let's keep this simple and just do it all white will have to be a bit more careful about the timings when doing RGB
-		/**
+		
 		for(int i=0; i<STRING_LEN; i++) {
-			value=(pixels[i] + errors[i]) >> shift;
-			put_pixel(urgb_u32(0, 0, value));
-			errors[i] = (pixels[i] + errors[i]) - (value << shift); // is this correct?
-		}
-		**/
-		for(int i=0; i<STRING_LEN; i++) {
-			value=(pixels[i] + errors[i]) >> shift;
-			//printf("pixels[i] = %d", pixels[i]);
-			put_pixel(urgb_u32(0, 0, value));
-			errors[i] = (pixels[i] + errors[i]) - (value << shift); // is this correct?
+			valueb=(pixelsb[i] + errorsb[i]) >> shift;
+			valuer=(pixelsr[i] + errorsr[i]) >> shift;
+			valueg=(pixelsg[i] + errorsg[i]) >> shift;
+
+			put_pixel(urgb_u32(valuer, valueg, valueb));
+			errorsb[i] = (pixelsb[i] + errorsb[i]) - (valueb << shift); 
+			errorsr[i] = (pixelsr[i] + errorsr[i]) - (valuer << shift); 
+			errorsg[i] = (pixelsg[i] + errorsg[i]) - (valueg << shift); 
 		}
 		sleep_us(400);
 	}
@@ -67,7 +70,9 @@ int main() {
     ws2812_program_init(pio, sm, offset, PIN_TX, 800000, false);
 	
 	for(int i=0; i< STRING_LEN; ++i) {
-		pixels[i]=0;
+		pixelsr[i]=0;
+		pixelsg[i]=0;
+		pixelsb[i]=0;
 	}
  
 	multicore_launch_core1(ws2812b_core);
@@ -75,16 +80,38 @@ int main() {
     while (1) {
         for (int i = 0; i < 30; ++i) {
 			
-			pixels[i] = i;
-			
-			for (int j=0;j<100;++j){
-				pixels[0] = j;
-				if(j%8 == 0) { pixels[1] = j; }
+			for (int j=0;j<30;++j){
+				pixelsb[0] = j;
+				if(j%8 == 0) { pixelsb[1] = j; }
+				pixelsr[2] = j;
+				if(j%8 == 0) { pixelsr[3] = j; }
+				pixelsg[4] = j;
+				if(j%8 == 0) { pixelsg[5] = j; }
+				pixelsg[6] = j;
+				pixelsr[6] = j;
+				pixelsb[6] = j;
+				if(j%8 == 0) { 
+					pixelsg[7] = j;
+					pixelsr[7] = j;
+					pixelsb[7] = j; 
+				}					
 				sleep_ms(50);
 			}
-			for (int j=100;j>0;--j){
-				pixels[0] = j;
-				if(j%8 == 0) { pixels[1] = j; }
+			for (int j=30;j>0;--j){
+				pixelsb[0] = j;
+				if(j%8 == 0) { pixelsb[1] = j; }
+				pixelsr[2] = j;
+				if(j%8 == 0) { pixelsr[3] = j; }
+				pixelsg[4] = j;
+				if(j%8 == 0) { pixelsg[5] = j; }
+				pixelsg[6] = j;
+				pixelsr[6] = j;
+				pixelsb[6] = j;
+				if(j%8 == 0) { 
+					pixelsg[7] = j;
+					pixelsr[7] = j;
+					pixelsb[7] = j; 
+				}					
 				sleep_ms(50);
 			}
         }
